dab840b08eda0039586280404c8b93f6f8eaa1fe
d777wgm <123156826+d777wgm@users.noreply.github.com>
2023-02-17 09:07:53 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+basepath = '/home/ifmt/daqhats/script' 
+mypath = basepath + '/HyWeld'
+csv_header = [['V1', 'V2']]
+scan_rate =100
+channels = [0, 1]
+channel_for_threshold = 1
+threshold = 10
+READ_ALL_AVAILABLE = -1
+
+
+
+
+
+
